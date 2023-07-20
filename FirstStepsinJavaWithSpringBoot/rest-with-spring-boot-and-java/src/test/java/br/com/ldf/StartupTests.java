07568fc748a6190121d41472f5c507c324fbc955
Victor Menezes <victormenezes.vicente@gmail.com>
2023-07-20 13:09:00 -0300

    First Steps in java with Spring Boot

---
+++ FirstStepsinJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/ldf/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.ldf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
